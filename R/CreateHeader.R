7cfdcaa02844be4d8b7e88e57db6efaec5d09305
smbjohnson <smbjohnson98@gmail.com>
2020-11-06 15:34:49 -0700

    Initial Commit

---
+++ R/CreateHeader.R
@@ -0,0 +1,21 @@
+#' A function that takes the first row of a dataframe and makes it the header
+#' @param dataframe This is a dataframe
+#' @import
+#' @return 'CreateHeader()' returns a dataframe where the previous first row has now become the header.
+#' @export
+#' @examples
+#' df <- data.frame(x = c("Height",180), y = c("Weight", 160))
+#' df
+#' CreateHeader(df)
+
+
+
+CreateHeader <- function(dataframe){
+
+  names(dataframe) <- dataframe[1,]
+
+  finaldf <- dataframe[-1,]
+
+  return(finaldf)
+
+}
