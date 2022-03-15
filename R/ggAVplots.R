f324117016aa64ba36bb26a4cdd4a159138b0570
smbjohnson <smbjohnson98@gmail.com>
2022-03-14 21:28:16 -0600

    Adding in an ggavplots function

---
+++ R/ggAVplots.R
@@ -0,0 +1,33 @@
+#' A function that takes a linear model and returns av plots that are ggplot objects
+#' @param model This is the model you want to check linearity for
+#' @param color This is the color of the dotted line
+#' @import
+#' @return 'ggAVplots()' return av plots for the variables in the model
+#' @export
+
+
+ggAVplots  <- function(MODEL, color = 'red') {
+  
+  #Extract the information for AV plots
+  AVPLOTS <- avPlots.invis(MODEL)
+  K       <- length(AVPLOTS)
+  
+  #Create the added variable plots using ggplot
+  GGPLOTS <- vector('list', K)
+  for (i in 1:K) {
+    DATA         <- data.frame(AVPLOTS[[i]])
+    GGPLOTS[[i]] <- ggplot2::ggplot(aes_string(x = colnames(DATA)[1], 
+                                               y = colnames(DATA)[2]), 
+                                    data = DATA) +
+      geom_point(colour = 'black') + 
+      geom_smooth(method = 'lm', se = FALSE, 
+                  color = color, formula = y ~ x, linetype = 'dashed') +
+      labs(
+        x = paste0('(', names(DATA)[1], ' | others)'),
+        y = '' #ylab(paste0('(',ifelse(is.null(YLAB), paste0(names(DATA)[2], ' | others'), YLAB), ')'))
+      )
+  }
+  
+  #Return output object
+  GGPLOTS }
+
