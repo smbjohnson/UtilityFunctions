856487814c13846f5eb9a8fb2cf1defceecf08ae
smbjohnson <smbjohnson98@gmail.com>
2022-06-24 00:52:04 -0600

    Adding in corrections, aestriks, and potential score

--- R/Wizard_Scorecard.R
+++ R/Wizard_Scorecard.R
@@ -74,15 +74,36 @@ wizard <- function(save = FALSE){
     if(j==1) { # score for the first round is going off 0
       scorecard %<>%
         dplyr::group_by(Player) %>%
-        dplyr::mutate(Score = ifelse(Bid == Actual, 20 + (10*Bid), -10*abs(Bid - Actual)))
+        dplyr::mutate(
+          Score = ifelse(Bid == Actual, 20 + (10*Bid), -10*abs(Bid - Actual)),
+          PotScore = 20 + (10 * Bid)
+          )
     } else { # scores are updated based on previous scores
       scorecard %<>%
         dplyr::group_by(Player) %>%
-        dplyr::mutate(Score = lag(Score, n = 1, default = 0, order_by = Round) + ifelse(Bid == Actual, 20 + (10 * Bid), -10*(abs(Bid - Actual))),
-               Diff = Score - lag(Score, n = 1, default = 0))
+        dplyr::mutate(
+               Score = lag(Score, n = 1, default = 0, order_by = Round) + ifelse(Bid == Actual, 20 + (10 * Bid), -10*(abs(Bid - Actual))),
+               Diff = Score - lag(Score, n = 1, default = 0),
+               PotScore = lag(PotScore, n = 1, default = 0, order_by = Round) + (20 + (10 * Bid)),
+               PotIncrease = PotScore - lag(PotScore, n = 1, default = 0)
+               )
     }
+    # checking for corrections
+    cor <- menu(c('Yes','No'), title = 'Are there any corrections?')
+    # inputing the correction
+    if(cor == 'Yes') {
+      other_cor == TRUE
+      while(other_cor == TRUE) {
+      player <- menu(player_name, title = "Whose score needs correction?")
+      bid_act <- menu(c('Bid','Actual'), title = "What it the bid or actual?")
+      true_val <- as.numeric(readline(prompt = "What is the true value?"))
+      scorecard[scorecard$Player == player & scorecard$Round == j,bid_act] <- true_val
+      other_cor <- menu(c(TRUE,FALSE), title = "Are there other corrections?")
+      }
+    }
+
     # printing off score update
-    print(scorecard %>% dplyr::filter(Round == j) %>% dplyr::select(Player,Score) %>% dplyr::arrange(desc(Score)))
+    print(scorecard %>% dplyr::filter(Round == j) %>% dplyr::select(Player,Score) %>% dplyr::arrange(desc(Score), Player))
     # updating the order for players bids
     player_order <- c(player_order[-1], player_order[1])
   }
@@ -92,7 +113,14 @@ wizard <- function(save = FALSE){
     game_date <- gsub(pattern = " ", replacement = "_", game_date)
     write.csv(scorecard, paste0("wizard_scorecard_",game_date,".csv"))
     print(paste0("scorecard saved to ", getwd()))
-    }
+  }
+  # asking if there should be an asterisk
+  asterisk <- menu(c('Yes','No'), title = "Should there be an asterisk")
+  if(asterisk == 'Yes') {
+    offender <- readline(prompt = "Who cheated?")
+    scorecard %<>%
+      mutate(asterisk = offender)
+  }
   # returning the scorecard
   return(scorecard)
 }
